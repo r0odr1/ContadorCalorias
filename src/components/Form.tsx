2fa2b61788a3c6999f2f60cc522f3e2eee6473e1
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-24 09:50:00 -0500

    formulario y categorias

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,48 @@
+import { categories } from "../data/categories"
 
 export default function Form() {
   return (
-    <div>Form</div>
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">Categoria:</label>
+        <select className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+        >
+          {categories.map(category => (
+            <option
+            key={category.id}
+            value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">Actividad:</label>
+        <input
+          id="activity"
+          type="text"
+          className="border border-slate-30 p-2 rounded-lg"
+          placeholder="Ej: correr, comer, etc"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">Calorias:</label>
+        <input
+          id="calories"
+          type="number"
+          className="border border-slate-30 p-2 rounded-lg"
+          placeholder="Calorias. Ej: 300"
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value="Guardar Comida o guardar Ejecricio"
+      />
+    </form>
   )
 }
