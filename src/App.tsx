9e7dc64e027560657071e1704edfa790b333ac73
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-24 16:01:42 -0500

    componente para crear el total de calorias

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer, useEffect, useMemo } from "react"
 import Form from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
 import ActivityList from "./components/ActivityList"
+import CalorieTracker from "./components/CalorieTracker"
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState)
@@ -37,6 +38,16 @@ function App() {
             dispatch={dispatch}
             state={state}
           />
+        </div>
+
+      </section>
+
+      <section className="bg-green-800 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker
+            activities={state.activities}
+          />
+
         </div>
 
       </section>
