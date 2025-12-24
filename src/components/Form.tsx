1e6eab3badc1bc4dfc151cdabb923e6232e33026
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-24 10:23:12 -0500

    actividad y seteando calores del state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,32 @@
+import { useState } from "react"
+import type { ChangeEvent } from "react"
+import type { Activity } from "../types"
 import { categories } from "../data/categories"
 
 export default function Form() {
+  const [activity, setActivity] = useState<Activity>({
+    category: 1,
+    name: '',
+    calories: 0
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const isNumberField = ['category', 'calories'].includes(e.target.id)
+
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value
+    })
+  }
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">Categoria:</label>
         <select className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
+          value={activity.category}
+          onChange={handleChange}
         >
           {categories.map(category => (
             <option
@@ -19,12 +39,14 @@ export default function Form() {
       </div>
 
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">Actividad:</label>
+        <label htmlFor="name" className="font-bold">Actividad:</label>
         <input
-          id="activity"
+          id="name"
           type="text"
           className="border border-slate-30 p-2 rounded-lg"
           placeholder="Ej: correr, comer, etc"
+          value={activity.name}
+          onChange={handleChange}
         />
       </div>
 
@@ -35,6 +57,8 @@ export default function Form() {
           type="number"
           className="border border-slate-30 p-2 rounded-lg"
           placeholder="Calorias. Ej: 300"
+          value={activity.calories}
+          onChange={handleChange}
         />
       </div>
 
