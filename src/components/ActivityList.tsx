debfde629354053e2121c0e5932f19a5519e9887
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-24 15:11:20 -0500

    mostrar mensaje si no hay actividades

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -14,47 +14,53 @@ export default function ActivityList({ activities, dispatch }: ActivityListprops
     (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''),
     [activities])
 
+    const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
+
   return (
     <>
       <h2 className="text-4xl font-bold text-slate-600 text-center">Comida y Actividades</h2>
-      {activities.map(activity => (
-        <div
-          key={activity.id}
-          className="px-5 py-10 bg-white mt-5 flex justify-between"
-        >
-          <div className="space-y-2 relative">
-            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold rounded-lg
-              ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500' }`}
-            >
-              {categoryName(+activity.category)}
-            </p>
-            <p className="text-2xl font-bold pt-5">{activity.name}</p>
-            <p className="font-black text-4xl text-lime-500">
-              {activity.calories} {''}
-              <span>Calorias</span>
-            </p>
+
+      {isEmptyActivities ?
+        <p className="text-center my-5">No hay actividades</p> :
+        activities.map(activity => (
+          <div
+            key={activity.id}
+            className="px-5 py-10 bg-white mt-5 flex justify-between"
+          >
+            <div className="space-y-2 relative">
+              <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold rounded-lg
+                ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500' }`}
+              >
+                {categoryName(+activity.category)}
+              </p>
+              <p className="text-2xl font-bold pt-5">{activity.name}</p>
+              <p className="font-black text-4xl text-lime-500">
+                {activity.calories} {''}
+                <span>Calorias</span>
+              </p>
+            </div>
+
+            <div className="flex gap-5 items-center">
+              <button
+                onClick={() => dispatch({type: "set-active-id", payload: {id: activity.id}})}
+              >
+                <PencilSquareIcon
+                  className="h-8 w-8 text-gray-800"
+                />
+              </button>
+
+              <button
+                onClick={() => dispatch({type: "delete-active", payload: {id: activity.id}})}
+              >
+                <XCircleIcon
+                  className="h-8 w-8 text-red-500"
+                />
+              </button>
+            </div>
+
           </div>
-
-          <div className="flex gap-5 items-center">
-            <button
-              onClick={() => dispatch({type: "set-active-id", payload: {id: activity.id}})}
-            >
-              <PencilSquareIcon
-                className="h-8 w-8 text-gray-800"
-              />
-            </button>
-
-            <button
-              onClick={() => dispatch({type: "delete-active", payload: {id: activity.id}})}
-            >
-              <XCircleIcon
-                className="h-8 w-8 text-red-500"
-              />
-            </button>
-          </div>
-
-        </div>
-      ))}
+        ))
+      }
     </>
   )
 }
