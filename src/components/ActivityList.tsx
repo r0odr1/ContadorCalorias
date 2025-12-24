7a3a0a1f29d6e0402bda3f6901bbef0fa2326535
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-24 15:57:03 -0500

    estilo a la actividades

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -25,7 +25,7 @@ export default function ActivityList({ activities, dispatch }: ActivityListprops
         activities.map(activity => (
           <div
             key={activity.id}
-            className="px-5 py-10 bg-white mt-5 flex justify-between"
+            className="px-5 py-10 bg-white mt-5 flex justify-between shadow rounded-lg"
           >
             <div className="space-y-2 relative">
               <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold rounded-lg
